f440dc398c635de859eaacd994e60e2e6be52a64
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-17 16:31:51 +0530

    entry file done

--- src/main/java/Item_Entries.java
+++ src/main/java/Item_Entries.java
@@ -3,4 +3,36 @@ public class Item_Entries {
     private String name;
     private String category;
     private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
