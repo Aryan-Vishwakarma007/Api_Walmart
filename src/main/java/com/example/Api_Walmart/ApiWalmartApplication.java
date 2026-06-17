15ba40af5e0a61f2efa2ac040929ec165af3494e
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-17 16:26:01 +0530

    Initial commit

---
+++ src/main/java/com/example/Api_Walmart/ApiWalmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.Api_Walmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWalmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWalmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Api_Walmart/ApiWalmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Api_Walmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWalmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
