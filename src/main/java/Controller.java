20ad4e9dfc2c02cf9d4197857d4f1ce4f97e391b
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-17 16:49:21 +0530

    done

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,16 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/search")
 public class Controller {
+    @GetMapping("id/{myId}")
+    public Item_Entries get_by_id(@PathVariable int myId){
+        return
+    }
+    @GetMapping("/param")
+    public Item_Entries get_by_category(@RequestParam String cat){
+        return
+    }
 }
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,2 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
+    private List<Item_Entries> avaliable_products = new ArrayList<>(List<Item_Entries>);
 }
