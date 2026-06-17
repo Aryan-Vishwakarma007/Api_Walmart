29affbb58e792c81c844cb8411c4aa00b4fdf488
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-17 16:54:38 +0530

    done

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -2,5 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
-    private List<Item_Entries> avaliable_products = new ArrayList<>(List<Item_Entries>);
+    private List<Item_Entries> products = new ArrayList<>(List.of(
+            new Item_Entries(1, "TV", "electronics", 499),
+            new Item_Entries(2, "Shirt", "clothing", 29),
+            new Item_Entries(3, "Laptop", "electronics", 999)
+    ));
 }
