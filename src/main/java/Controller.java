cbe9aa5adbaf36d5e54eb4f945766cb37d404786
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-17 17:00:50 +0530

    done

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,16 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @RequestMapping("/search")
 public class Controller {
+
+    @Autowired
+    private Service sr;
+
     @GetMapping("id/{myId}")
     public Item_Entries get_by_id(@PathVariable int myId){
         return
